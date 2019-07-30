f30c3d1e6fbb828504ec16adfaea5fe7b9330ce1
Billy Vlachos <williamcaminiti@live.com>
2019-07-30 17:45:55 +0200

    Add home nav link and breathe app

--- src/app/breate/breate.module.ts
+++ src/app/breate/breate.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreateComponent } from './breate.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: BreateComponent }
+    ])
   ],
   declarations: [BreateComponent],
   exports: [BreateComponent]
