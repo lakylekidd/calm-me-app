cd1259f9afc6b8819689f70aa81208ae15515c2a
Billy Vlachos <williamcaminiti@live.com>
2019-07-31 11:21:21 +0200

    Add event listeners to track when user clicks on screen, to remove user data menu

--- src/app/layout/header/nav/nav.component.ts
+++ src/app/layout/header/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, HostListener } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthService } from "app/services/auth.service";
 import { JwtPayload } from "app/auth/helpers/jwt";
@@ -17,10 +17,22 @@ export class NavComponent implements OnInit {
 	constructor(
 		private readonly authService: AuthService,
 		private readonly router: Router
-	) {}
+	) {
+		this.getScreenSize();
+	}
+
+	checkIfMobile = () => {
+		if (this.scrWidth <= 1200) {
+			this.displayUserMenu = true;
+		}
+	};
 
 	toggleOverlay() {
 		this.overlay = !this.overlay;
+	}
+
+	toggleUserMenu(e) {
+		this.displayUserMenu = !this.displayUserMenu;
 	}
 
 	handleLoginClick = () => {
@@ -36,5 +48,31 @@ export class NavComponent implements OnInit {
 			this.authenticated = v !== null;
 			this.userData = v;
 		});
+	}
+
+	// Declare height and width variables
+	scrHeight: any;
+	scrWidth: any;
+
+	/**
+	 * Will retrieve the window size and save it as a variable
+	 * @param event 
+	 */
+	@HostListener("window:resize", [ "$event" ])
+	getScreenSize(event?) {
+		this.scrHeight = window.innerHeight;
+		this.scrWidth = window.innerWidth;
+		this.checkIfMobile();
+	}
+
+	/**
+	 * Will check if the user clicked anywhere on the window
+	 * @param event 
+	 */
+	@HostListener("document:click", [ "$event" ])
+	documentClick(event) {
+		if (event.target.id !== "user-data-toggler") {
+			this.displayUserMenu = false;
+		}
 	}
 }
