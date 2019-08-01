93c335002571b51de55394be861599ea43c7881a
Billy Vlachos <williamcaminiti@live.com>
2019-08-01 03:34:06 +0200

    Update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
+const CI = process.env["CI"] === "true";
 
 module.exports = function(config) {
 	config.set({
@@ -8,33 +9,27 @@ module.exports = function(config) {
 		plugins: [
 			require("karma-jasmine"),
 			require("karma-chrome-launcher"),
-			require("karma-remap-istanbul"),
+			require("karma-jasmine-html-reporter"),
+			require("karma-coverage-istanbul-reporter"),
 			require("@angular-devkit/build-angular/plugins/karma")
 		],
-		//files: [],
-		// preprocessors: {
-		// 	"./src/test.ts": [ "@angular-devkit/build-angular" ]
-		// },
-		mime: {
-			"text/x-typescript": [ "ts", "tsx" ]
+		client: {
+			clearContext: false // leave Jasmine Spec Runner output visible in browser
 		},
-		remapIstanbulReporter: {
+		coverageIstanbulReporter: {
 			dir: require("path").join(__dirname, "coverage"),
-			reports: {
-				html: "coverage",
-				lcovonly: "./coverage/coverage.lcov"
-			}
+			reports: [ "html", "lcovonly" ],
+			fixWebpackSourcePaths: true
 		},
-
-		reporters:
-			config.angularCli && config.angularCli.codeCoverage
-				? [ "progress", "karma-remap-istanbul" ]
-				: [ "progress" ],
+		angularCli: {
+			environment: "dev"
+		},
+		reporters: [ "progress", "kjhtml" ],
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: [ "Chrome" ],
-		singleRun: false
+		browsers: [ CI ? "ChromeHeadless" : "Chrome" ],
+		singleRun: CI
 	});
 };
