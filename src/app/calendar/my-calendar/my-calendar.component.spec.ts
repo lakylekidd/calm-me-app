2951b0b8ddc6a418524cf40be14fcab43c0bc580
Billy Vlachos <williamcaminiti@live.com>
2019-08-01 04:00:45 +0200

    Calendar create error fix

--- src/app/calendar/my-calendar/my-calendar.component.spec.ts
+++ src/app/calendar/my-calendar/my-calendar.component.spec.ts
@@ -1,28 +1,31 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { DebugElement } from "@angular/core";
 
-import { MyCalendarComponent } from './my-calendar.component';
+import { MyCalendarComponent } from "./my-calendar.component";
+import { EventCalendarModule } from "app/event-calendar/event-calendar.module";
 
-describe('MyCalendarComponent', () => {
-  let component: MyCalendarComponent;
-  let fixture: ComponentFixture<MyCalendarComponent>;
+describe("MyCalendarComponent", () => {
+	let component: MyCalendarComponent;
+	let fixture: ComponentFixture<MyCalendarComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MyCalendarComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(
+		async(() => {
+			TestBed.configureTestingModule({
+				declarations: [ MyCalendarComponent ],
+				imports: [ EventCalendarModule ]
+			}).compileComponents();
+		})
+	);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MyCalendarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(MyCalendarComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
 });
